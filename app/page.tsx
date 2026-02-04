ffc1a08066c09264f2d08cde9218c91373d35f94
Yaghs <syaghinloo@gmail.com>
2026-02-03 19:11:23 -0500

    added JSX inside the home page and tailwind css for styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,32 @@ import Image from "next/image"
 
 export default function Home():JSX.Element {
   return (
-    <div className="bg-black">
-      <nav className="bg-bl">
+   <main>
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            YOUR GO-TO PLATFORM FOR 3D PRINTING FILES
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Discover what&apos;s possible with 3D printing
+          </h1>
+          <p className="text-lg text-gray-600"
+          >Join our community of creators and explore a vast library of user-submitted models.
+          </p>
+            <div className="flex gap-4">
+              <button
+              className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
+              >Browse Models</button>
+            </div>
+          </div>
+        
         <Image
-          src="/image.png"
-          alt="logo-image"
-          width={200}
-          height = {50}
+          src="/3D-printed.png"
+          alt="3d-printed-image"
+          width = {627}
+          height = {627}
         />
-        <a href="#">Models</a>
-        <a href="#">About</a>
-      </nav>
-    </div>
+      </section>
+    </main>
   );
 }
